5b709b6b689daf463ab190070a140488c70fcc0c
Trevor Xiao <tterris@redhat.com>
2020-09-18 14:59:43 -0400

    Update SDK version and change media check logic
    Instead of checking for unclearable notifications, check if media is playing, multiple audio devices are attached, or google maps is running.

--- app/src/main/java/com/txiao/mutemedia/PhoneNotificationListener.java
+++ app/src/main/java/com/txiao/mutemedia/PhoneNotificationListener.java
@@ -5,18 +5,30 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.AudioDeviceInfo;
 import android.media.AudioManager;
 import android.os.IBinder;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by txiao on 12/14/16.
  */
 
 public class PhoneNotificationListener extends NotificationListenerService {
+
+    private Integer[] internalAudioDevicesArray = {AudioDeviceInfo.TYPE_BUILTIN_EARPIECE,
+            AudioDeviceInfo.TYPE_BUILTIN_SPEAKER,
+            AudioDeviceInfo.TYPE_BUILTIN_SPEAKER_SAFE,
+            AudioDeviceInfo.TYPE_TELEPHONY};
+    private List<Integer> internalAudioDevices = Arrays.asList(internalAudioDevicesArray);
 
     private BroadcastReceiver mPowerKeyReceiver = new BroadcastReceiver() {
         @Override
@@ -50,15 +62,16 @@ public class PhoneNotificationListener extends NotificationListenerService {
 
     private void muteMediaIfUnused(Context context) {
 
-        boolean allNotificationsClearable = true;
+        boolean mapsRunning = false;
         for (StatusBarNotification notification : this.getActiveNotifications()) {
-            if (!notification.isClearable()) {
-                allNotificationsClearable = false;
+            if ("com.google.android.apps.maps".equals(notification.getPackageName())) {
+                mapsRunning = true;
             }
         }
 
-        if (allNotificationsClearable) {
-            AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        //if no music is playing and we're not connected to a speaker other than phone audio, mute
+        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        if (!audioManager.isMusicActive() && !externalAudioDevices(audioManager) && !mapsRunning) {
             audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0);
         }
     }
@@ -66,5 +79,13 @@ public class PhoneNotificationListener extends NotificationListenerService {
     private boolean locked() {
         KeyguardManager km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
         return km.inKeyguardRestrictedInputMode();
+    }
+
+    private boolean externalAudioDevices(AudioManager audioManager) {
+        boolean externalAudioDevices = false;
+        for (AudioDeviceInfo device : audioManager.getDevices(AudioManager.GET_DEVICES_OUTPUTS)) {
+            externalAudioDevices = externalAudioDevices || !internalAudioDevices.contains(device.getType());
+        }
+        return externalAudioDevices;
     }
 }
